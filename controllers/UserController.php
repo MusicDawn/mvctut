1c31940b96f60d760f4d31d3b3c454664535e3eb
MusicDawn <panagiotis.d.kostakis@gmail.com>
2024-11-25 18:11:36 +0200

    Controller v2

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -6,11 +6,18 @@
 $errorbool1 = false;
 $errorbool2 = false;
 
-// This code will be excuted when we press Submit
-if (isset($_POST["submit"])) {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    createUser($con, $first_name, $last_name, $email);
+function create($con)
+{
+    // This code will be excuted when we press Submit
+    if (isset($_POST["submit"])) {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        createUser($con, $first_name, $last_name, $email);
+    }
+}
+
+function success(){
+    require_once('views/success.php');
 }
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once('env.php');
 require_once('mysqlconnect.php');
 require_once('models/UserModel.php');
 require_once('controllers/UserController.php');
+create($con); // the funtion create($con) is defined in UserController.php
 ?>
 
 <!DOCTYPE html>
@@ -19,9 +20,9 @@ require_once('controllers/UserController.php');
 <body>
     <?php
     // We change the header location [if ($statement->execute()...] to index.php and we from now on we go to 'success.php' from those ifs.
-    if($_SERVER['REQUEST_URI']=="/index.php") require_once('views/success.php');
+    if($_SERVER['REQUEST_URI']=="/index.php") success(); // the funtion success is defined in UserController.php
     else require_once('views/home.php');
-    
+
 
     //This is so we can see at the bottom of the page the $_SERVER['REQUEST_METHOD'] && $_SERVER['REQUEST_URI'].
     echo '<pre style="position: absolute; bottom :0; left: 5px;" >';
