2e7c515cf24745f27f5e5482e14c45dcd63df399
MusicDawn <panagiotis.d.kostakis@gmail.com>
2024-11-25 18:41:10 +0200

    MVC v1

--- index.php
+++ index.php
@@ -23,18 +23,17 @@ $creator->create($con);
 <body>
     <?php
     // We change the header location [if ($statement->execute()...] to index.php and we from now on we go to 'success.php' from those ifs.
-    if($_SERVER['REQUEST_URI']=="/index.php") $creator->success(); // the function success is defined in the class UserController
-    else require_once('app/views/home.php');
-
-
-    //This is so we can see at the bottom of the page the $_SERVER['REQUEST_METHOD'] && $_SERVER['REQUEST_URI'].
-    echo '<pre style="position: absolute; bottom :0; left: 5px;" >';
-    echo ' Method ';
-    print_r($_SERVER['REQUEST_METHOD']);
-    echo '<br><br> URI : ';
-    print_r($_SERVER['REQUEST_URI']);
-    echo '</pre>';
+    if ($_SERVER['REQUEST_URI'] == "/index.php")
+        $creator->success(); // the function success is defined in the class UserController
+    else
+        require_once('app/views/home.php');
     ?>
+
+    <pre style="position: absolute; bottom :0; left: 5px;">
+    <!-- This is so we can see at the bottom of the page the $_SERVER['REQUEST_METHOD'] && $_SERVER['REQUEST_URI']. -->
+     Method : <?php echo ($_SERVER['REQUEST_METHOD']); ?>
+     URI :<?php echo ($_SERVER['REQUEST_URI']); ?>       
+    </pre>
 </body>
 
 </html>
