9f4c83d5c31c45633a27402911fce8cba35bef67
MusicDawn <panagiotis.d.kostakis@gmail.com>
2024-12-10 19:06:18 +0200

    Router with try/catch done! ep 14

--- routes/Routes.php
+++ routes/Routes.php
@@ -2,6 +2,8 @@
 
 namespace RouterSpace;
 
+use Exception;
+
 class Routes
 {
     private $routes = [];
@@ -9,15 +11,16 @@ class Routes
     public function addRoutes($uri, $controller, $method)
     {
         $this->routes[$uri] = ['controller' => $controller, 'method' => $method];
-        if (array_key_exists($_SERVER['REQUEST_URI'], $this->routes)) {
+        try {
+            if (!array_key_exists($_SERVER['REQUEST_URI'], $this->routes)) throw new Exception("URI Does not exist!");
             $controller = $this->routes[$uri]['controller'];
-            if (class_exists($controller)) {
-                $method = $this->routes[$uri]['method'];
-                $inst = new $controller;
-                if (method_exists($inst, $method)) {
-                    $inst->$method();
-                } else echo "Method Does not exist!";
-            } else echo "Class Name Does not exist!";
-        } else echo "URI Does not exist!";
+            if (!class_exists($controller)) throw new Exception("Class Name Does not exist!");
+            $method = $this->routes[$uri]['method'];
+            $inst = new $controller;
+            if (!method_exists($inst, $method)) throw new Exception("Method Does not exist!");
+            $inst->$method();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
