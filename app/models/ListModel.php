cbd657438aef76a0b03de00c54a4ada5134347c8
MusicDawn <panagiotis.d.kostakis@gmail.com>
2024-12-24 12:46:24 +0200

    Add prepare $stmt into List Model ep 18.

--- app/models/ListModel.php
+++ app/models/ListModel.php
@@ -21,8 +21,12 @@ class ListModel
     public function single($con, $id)
     {
         try {
-            $sql = "SELECT * FROM users WHERE id=$id"; 
-            $result = $con->query($sql);
+            // We using prepare/bind_para/execute so we dont get hacked!
+            $sql = "SELECT * FROM users WHERE id=?"; 
+            $stm = $con->prepare($sql);
+            $stm->bind_param("s", $id);
+            $stm->execute();
+            $result = $stm->get_result();
             if($con->error) throw new Exception("Datababase Error: " . $con->error); 
             return $result;
         } catch (Exception $e) {
